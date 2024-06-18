24ae547324a85a51edafab3863e5ef4ed2cdc801
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2024-06-18 12:26:24 +0700

    implement webpack to handle dependencies

--- src/assets/js/component.js
+++ src/assets/js/component.js
@@ -25,3 +25,5 @@ const navbar = {
         `
     }
 }
+
+window.navbar = navbar;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './assets/css/brand.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'jquery/dist/jquery.js';
+import './assets/js/favicon.js';
+import './assets/js/font.js';
+import './assets/js/component.js';
