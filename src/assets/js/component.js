ed95c6db43811b6ce5cb1aa3705ae70c8ce7263b
Wahyu Wijiyanto <wejheyanto95@gmail.com>
2024-06-18 17:15:30 +0700

    add summary component

--- src/assets/js/component.js
+++ src/assets/js/component.js
@@ -9,8 +9,8 @@ const navbar = {
     inflate: function(elementId) {
         document.getElementById(elementId).innerHTML += `
         <nav class="navbar ${this.background}">
-            <div class="container">
-                <div class="container-fluid">
+            <div class="container-fluid">
+                <div class="container">
                     <div class="row" style="margin-top: 0.16rem;">
                         <div class="col ${this.color} fs-4 fw-bold d-flex align-items-center justify-content-start mb-1">
                             <img src="${this.img}" height="40px" class="d-inline me-3">
@@ -28,4 +28,39 @@ const navbar = {
     }
 }
 
+const summary = {
+    numOfActiveGoals: "3",
+    overallProgress: "85",
+
+    inflate: function(elementId) {
+        document.getElementById(elementId).innerHTML += `
+        <div class="cotainer-fluid py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col me-5">
+                        <h1 class="fw-bold text-shrink">You have ${this.numOfActiveGoals} active goals!</h1>
+                        <p>This is some text within a card body.</p>
+                        <button type="button" class="btn btn-brand rounded-3 me-2">
+                            <i class="bi bi-plus-lg me-2"></i>
+                            Add New Goal
+                        </button>
+                        <button type="button" class="btn btn-light">Reset Progress</button>
+                    </div>
+                    <div class="col card text-ice rounded-4 border border-0 bg-brand-svg ms-5">
+                        <div class="card-body">
+                            <h4 class="fw-bold">Overall Progress</h4>
+                            <p>You've done ${this.overallProgress}% of your entire milestones!</p>
+                            <div class="progress mt-3 mb-2" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: ${this.overallProgress}%"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
 window.navbar = navbar;
+window.summary = summary;
